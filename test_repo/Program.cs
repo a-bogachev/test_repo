342cc3cf9e4b56ef5caa39c7e135a7af9d88c35e
Unknown <a.bogachev@apptimized.com>
2018-01-17 16:51:11 +0200

    fibonacci alghorithm

--- test_repo/Program.cs
+++ test_repo/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Numerics;
 namespace test_repo
 {
     class Program
     {
+        public static BigInteger fib(BigInteger n)
+        {
+            BigInteger res = 0, i=0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            res = 2;
+            BigInteger a = 1, b = 1, c=0;
+            for (i=3; i<=n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                res += c;
+            }
+            return res;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            BigInteger a;
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Program.fib(a));
             Console.ReadKey();
-            Console.WriteLine("I like GitKraken");
-            
         }
     }
 }
